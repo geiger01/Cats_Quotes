7e6548e0597b25e0ea7566d27fddaf42813a49f3
Jonathan Geiger <78149229+geiger01@users.noreply.github.com>
2021-06-13 11:49:29 +0300

    Update app.js

--- CatsQuotesAPI/app.js
+++ CatsQuotesAPI/app.js
@@ -6,8 +6,8 @@ const url = "https://cat-fact.herokuapp.com/facts";
 button.addEventListener("click", getQuote);
 
 async function getQuote() {
-  const something = await fetch(url);
-  const data = await something.json();
+  const response = await fetch(url);
+  const data = await response.json();
   let randomNumber = Math.floor(Math.random() * 6);
   console.log(data);
 
